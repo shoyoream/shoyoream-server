891f7735e6432e5cb3e0ddd2fe6c8f3868c85a14
moonpiderman <lotsofchuck@gmail.com>
2023-10-24 17:32:56 +0900

    Add: Spring Boot Data Redis 추가 및 캐싱

---
+++ core/src/main/kotlin/shoyoream/server/shoyoreamapplication/core/infra/CachingConfig.kt
@@ -0,0 +1,8 @@
+package shoyoream.server.shoyoreamapplication.core.infra
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CachingConfig
