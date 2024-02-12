a9fd0dc0edf095d8c02fdc3f0c15bbfae0348d57
Bohyeon Moon <70942197+moonpiderman@users.noreply.github.com>
2024-02-12 22:04:53 +0900

    Feat: 프로듀서, 컨슈머 생성 (#16)

---
+++ core/src/main/kotlin/shoyoream/server/shoyoreamapplication/core/infra/config/KafkaConsumerConfig.kt
@@ -0,0 +1,36 @@
+package shoyoream.server.shoyoreamapplication.core.infra.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
+
+@Configuration
+class KafkaConsumerConfig(
+    @Value("\${spring.kafka.consumer.bootstrap-servers}") private val bootStrapSever: String,
+    @Value("\${spring.kafka.consumer.group-id}") private val groupId: String
+) {
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> {
+        val config: MutableMap<String, Any> = HashMap()
+        config[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootStrapSever
+        config[ConsumerConfig.GROUP_ID_CONFIG] = groupId
+        config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        config[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+
+        return DefaultKafkaConsumerFactory(config)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+}
