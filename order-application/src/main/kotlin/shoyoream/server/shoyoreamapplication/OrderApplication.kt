a3f6c984c99e185edbb73c5ec1cf2e132ace7bea
moonpiderman <lotsofchuck@gmail.com>
2024-02-17 23:25:54 +0900

    Wait: docker-compose 준비

--- order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/OrderApplication.kt
+++ order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/OrderApplication.kt
@@ -2,8 +2,10 @@ package shoyoream.server.shoyoreamapplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("classpath:/secure.properties")
 class OrderApplication
 
 fun main(args: Array<String>) {
