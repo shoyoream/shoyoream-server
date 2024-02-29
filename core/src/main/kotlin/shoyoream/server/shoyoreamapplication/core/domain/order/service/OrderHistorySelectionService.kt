b71d21ac436d4508f1729aa9b076aef02f011e0f
moonpiderman <lotsofchuck@gmail.com>
2024-02-29 13:24:00 +0900

    Feat: Stock 테스트 코드 및 OrderHistory 삭제

--- core/src/main/kotlin/shoyoream/server/shoyoreamapplication/core/domain/order/service/OrderHistorySelectionService.kt
+++
@@ -1,9 +0,0 @@
-package shoyoream.server.shoyoreamapplication.core.domain.order.service
-
-import org.springframework.stereotype.Service
-import shoyoream.server.shoyoreamapplication.core.domain.order.repository.OrderHistoryRepository
-
-@Service
-class OrderHistorySelectionService(
-    private val orderHistoryRepository: OrderHistoryRepository
-)
