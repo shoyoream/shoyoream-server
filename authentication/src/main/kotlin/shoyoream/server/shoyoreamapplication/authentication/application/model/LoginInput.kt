37ec74ab4fdd0c76dd32cc400b9d3be275f2ecff
moonpiderman <lotsofchuck@gmail.com>
2024-03-29 18:31:46 +0900

    Feat: GraphQL에서 HttpServletRequest

---
+++ authentication/src/main/kotlin/shoyoream/server/shoyoreamapplication/authentication/application/model/LoginInput.kt
@@ -0,0 +1,6 @@
+package shoyoream.server.shoyoreamapplication.authentication.application.model
+
+data class LoginInput(
+    val email: String,
+    val password: String
+)
--- authentication/src/main/kotlin/shoyoream/server/shoyoreamapplication/authentication/token/entity/RefreshToken.kt
+++ authentication/src/main/kotlin/shoyoream/server/shoyoreamapplication/authentication/token/entity/RefreshToken.kt
@@ -8,4 +8,5 @@ class RefreshToken(
     @Id
     val id: Long = 0L
     // id 로 설정해야 @EnableRedisRepositories 가 동작함
+
 )
