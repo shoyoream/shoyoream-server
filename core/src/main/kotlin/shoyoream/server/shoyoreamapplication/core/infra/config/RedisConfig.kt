56d044be4db670fa15f15c8d1fe39f95a5ebe5cb
moonpiderman <lotsofchuck@gmail.com>
2024-05-03 15:58:10 +0900

    Wait: Redis serializer 에러..

--- core/src/main/kotlin/shoyoream/server/shoyoreamapplication/core/infra/config/RedisConfig.kt
+++ core/src/main/kotlin/shoyoream/server/shoyoreamapplication/core/infra/config/RedisConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @Configuration
@@ -22,18 +24,19 @@ class RedisConfig(
         return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
-    @Bean
-    fun redisTemplate(): RedisTemplate<*, *> {
-        val redisTemplate = RedisTemplate<ByteArray, ByteArray>()
-        redisTemplate.connectionFactory = redisConnectionFactory()
-        return redisTemplate
-    }
-
 //    @Bean
-//    fun redisTemplate(): RedisTemplate<String, Any> {
-//        val template = RedisTemplate<String, Any>()
-//        template.keySerializer = StringRedisSerializer()
-//        template.valueSerializer = Jackson2JsonRedisSerializer(Any::class.java)
-//        return template
+//    fun redisTemplate(): RedisTemplate<*, *> {
+//        val redisTemplate = RedisTemplate<ByteArray, ByteArray>()
+//        redisTemplate.connectionFactory = redisConnectionFactory()
+//        return redisTemplate
 //    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<Any, Any> {
+        val template = RedisTemplate<Any, Any>()
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = Jackson2JsonRedisSerializer(Any::class.java)
+        template.connectionFactory = redisConnectionFactory()
+        return template
+    }
 }
