9e569383895c479180a5221028754054e9bf5270
moonpiderman <lotsofchuck@gmail.com>
2024-05-08 00:41:31 +0900

    Feat: google proto 메세지 생성

--- payment-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/service/PayAppService.kt
+++ payment-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/service/PayAppService.kt
@@ -3,9 +3,11 @@ package shoyoream.server.shoyoreamapplication.application.service
 import java.util.UUID
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import payments.protobuf.PaymentMessage
 import shoyoream.server.shoyoreamapplication.application.client.service.PayClientStrategyService
 import shoyoream.server.shoyoreamapplication.application.dto.PayRequest
 import shoyoream.server.shoyoreamapplication.core.common.constant.DefaultResponse
+import shoyoream.server.shoyoreamapplication.core.domain.order.entity.OrderStatus
 
 @Service
 class PayAppService(
@@ -14,6 +16,11 @@ class PayAppService(
     @Transactional
     fun pay(payRequest: PayRequest): DefaultResponse<UUID> {
         // TODO : PayType 에 따라 결제 분기처리 하기
+        // TODO : 결제 완료시에 해당 order 정보 업데이트 보내기
+        val orderStatusMessage = PaymentMessage.PaymentSuccessMessage.newBuilder()
+            .setOrderId(payRequest.orderId.toString())
+            .setUpdatedStatus(OrderStatus.PAYMENT_COMPLETED.name)
+            .build()
 
         return DefaultResponse.uuidResponse(UUID.randomUUID())
     }
--- payment-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/resolver/PayResolver.kt
+++ payment-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/resolver/PayResolver.kt
@@ -3,6 +3,7 @@ package shoyoream.server.shoyoreamapplication.resolver
 import java.util.UUID
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import shoyoream.server.shoyoreamapplication.application.dto.PayRequest
 import shoyoream.server.shoyoreamapplication.application.service.PayAppService
@@ -17,5 +18,12 @@ class PayResolver(
         @Argument payRequest: PayRequest
     ): DefaultResponse<UUID> {
         return payAppService.pay(payRequest)
+    }
+
+    @QueryMapping
+    fun getPayment(
+        @Argument paymentId: UUID
+    ): DefaultResponse<UUID> {
+        return DefaultResponse.uuidResponse(paymentId)
     }
 }
