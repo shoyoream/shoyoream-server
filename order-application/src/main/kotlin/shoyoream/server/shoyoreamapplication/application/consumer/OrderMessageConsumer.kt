6d72b62eeaee2e3a7eb65b8b33541fa851fb794c
moonpiderman <lotsofchuck@gmail.com>
2024-05-16 13:21:41 +0900

    Fix: Producing, Consuming 성공

--- order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/consumer/OrderMessageConsumer.kt
+++ order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/consumer/OrderMessageConsumer.kt
@@ -1,32 +1,31 @@
 package shoyoream.server.shoyoreamapplication.application.consumer
 
 import com.google.protobuf.InvalidProtocolBufferException
-import java.util.*
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import payments.protobuf.PaymentMessage
-import shoyoream.server.shoyoreamapplication.core.common.exception.DataNotFoundException
-import shoyoream.server.shoyoreamapplication.core.domain.order.entity.OrderStatus
-import shoyoream.server.shoyoreamapplication.core.domain.order.exception.OrderErrorType
 import shoyoream.server.shoyoreamapplication.core.domain.order.service.OrderSelectionService
 
 @Component
 class OrderMessageConsumer(
     private val orderSelectionService: OrderSelectionService,
-    private val kafkaTemplate: KafkaTemplate<UUID, Any>
+    private val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
     // TODO : 결제에 따른 리스너 생성
     @Transactional
-    @KafkaListener(topics = ["order-status"])
-    fun listenOrderStatus(message: Any) {
+    @KafkaListener(topics = ["order-status"], containerFactory = "orderListenerContainerFactory")
+    fun listenOrderStatus(message: ConsumerRecord<String, Any>) {
         try {
-            val paymentSuccessMessage = PaymentMessage.PaymentSuccessMessage.parseFrom(message as ByteArray)
-            val targetOrder = orderSelectionService.findOrderById(UUID.fromString(paymentSuccessMessage.orderId))
-                ?: throw DataNotFoundException(OrderErrorType.NOT_FOUND_ORDER)
+            val paymentSuccessMessage = PaymentMessage.PaymentSuccessMessage.parseFrom(message.value() as ByteArray)
 
-            targetOrder.updateOrderStatus(OrderStatus.valueOf(paymentSuccessMessage.updatedStatus))
+            println(paymentSuccessMessage.toString())
+//            val targetOrder = orderSelectionService.findOrderById(UUID.fromString(paymentSuccessMessage.orderId))
+//                ?: throw DataNotFoundException(OrderErrorType.NOT_FOUND_ORDER)
+//
+//            targetOrder.updateOrderStatus(OrderStatus.valueOf(paymentSuccessMessage.updatedStatus))
         } catch (e: InvalidProtocolBufferException) {
             throw e
         }
--- order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/infra/mq/OrderConsumerConfig.kt
+++ order-application/src/main/kotlin/shoyoream/server/shoyoreamapplication/application/infra/mq/OrderConsumerConfig.kt
@@ -1,7 +1,8 @@
-package shoyoream.server.shoyoreamapplication.infra.mq
+package shoyoream.server.shoyoreamapplication.application.infra.mq
 
-import java.util.UUID
+import kotlin.collections.HashMap
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -9,28 +10,27 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.support.serializer.JsonDeserializer
 
 @Configuration
-class PaymentConsumerConfig(
+class OrderConsumerConfig(
     @Value("\${spring.kafka.consumer.bootstrap-servers}") private val bootStrapSever: String,
     @Value("\${spring.kafka.consumer.group-id}") private val groupId: String
 ) {
     @Bean
-    fun consumerFactory(): ConsumerFactory<UUID, Any> {
+    fun consumerFactory(): ConsumerFactory<String, Any> {
         val config: MutableMap<String, Any> = HashMap()
         config[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootStrapSever
         config[ConsumerConfig.GROUP_ID_CONFIG] = groupId
         config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ByteArrayDeserializer::class.java
         config[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
 
         return DefaultKafkaConsumerFactory(config)
     }
 
     @Bean
-    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<UUID, Any> {
-        val factory = ConcurrentKafkaListenerContainerFactory<UUID, Any>()
+    fun orderListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
         factory.consumerFactory = consumerFactory()
         return factory
     }
